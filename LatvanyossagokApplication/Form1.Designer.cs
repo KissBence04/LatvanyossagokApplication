ff1b3bf9c48d8fa1d956e64322f556d8318c1fdd
Kiss Bence <kissbence2000@gmail.com>
2019-12-07 08:51:30 +0100

    Város hozzáadás

--- LatvanyossagokApplication/Form1.Designer.cs
+++ LatvanyossagokApplication/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbVnev = new System.Windows.Forms.TextBox();
+            this.lblVnev = new System.Windows.Forms.Label();
+            this.nUDLakossag = new System.Windows.Forms.NumericUpDown();
+            this.lblLakossag = new System.Windows.Forms.Label();
+            this.btnVarosHozzaAd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDLakossag)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbVnev
+            // 
+            this.tbVnev.Location = new System.Drawing.Point(88, 54);
+            this.tbVnev.Name = "tbVnev";
+            this.tbVnev.Size = new System.Drawing.Size(120, 20);
+            this.tbVnev.TabIndex = 0;
+            // 
+            // lblVnev
+            // 
+            this.lblVnev.AutoSize = true;
+            this.lblVnev.Location = new System.Drawing.Point(12, 57);
+            this.lblVnev.Name = "lblVnev";
+            this.lblVnev.Size = new System.Drawing.Size(64, 13);
+            this.lblVnev.TabIndex = 1;
+            this.lblVnev.Text = "Város neve:";
+            // 
+            // nUDLakossag
+            // 
+            this.nUDLakossag.Location = new System.Drawing.Point(88, 107);
+            this.nUDLakossag.Name = "nUDLakossag";
+            this.nUDLakossag.Size = new System.Drawing.Size(120, 20);
+            this.nUDLakossag.TabIndex = 2;
+            // 
+            // lblLakossag
+            // 
+            this.lblLakossag.AutoSize = true;
+            this.lblLakossag.Location = new System.Drawing.Point(12, 109);
+            this.lblLakossag.Name = "lblLakossag";
+            this.lblLakossag.Size = new System.Drawing.Size(56, 13);
+            this.lblLakossag.TabIndex = 3;
+            this.lblLakossag.Text = "Lakosság:";
+            // 
+            // btnVarosHozzaAd
+            // 
+            this.btnVarosHozzaAd.Location = new System.Drawing.Point(15, 160);
+            this.btnVarosHozzaAd.Name = "btnVarosHozzaAd";
+            this.btnVarosHozzaAd.Size = new System.Drawing.Size(98, 23);
+            this.btnVarosHozzaAd.TabIndex = 4;
+            this.btnVarosHozzaAd.Text = "Város hozzáad";
+            this.btnVarosHozzaAd.UseVisualStyleBackColor = true;
+            this.btnVarosHozzaAd.Click += new System.EventHandler(this.btnVarosHozzaAd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVarosHozzaAd);
+            this.Controls.Add(this.lblLakossag);
+            this.Controls.Add(this.nUDLakossag);
+            this.Controls.Add(this.lblVnev);
+            this.Controls.Add(this.tbVnev);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nUDLakossag)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbVnev;
+        private System.Windows.Forms.Label lblVnev;
+        private System.Windows.Forms.NumericUpDown nUDLakossag;
+        private System.Windows.Forms.Label lblLakossag;
+        private System.Windows.Forms.Button btnVarosHozzaAd;
     }
 }
 
--- LatvanyossagokApplication/Form1.cs
+++ LatvanyossagokApplication/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,39 @@ namespace LatvanyossagokApplication
 {
     public partial class Form1 : Form
     {
+        MySqlConnection conn;
         public Form1()
         {
             InitializeComponent();
+
+            conn = new MySqlConnection("Server=localhost;Port=3306;Database=latvanyossagokdb;Uid=root;Pwd=;");
+            conn.Open();
+        }
+
+        public void TablaLetrehoz()
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS varosok (ID INTEGER AUTO_INCREMENT PRIMARY KEY, NEV VARCHAR(255) NOT NULL, LAKOSSAG INTEGER NOT NULL)";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS latvanyossag (ID INTEGER AUTO_INCREMENT PRIMARY KEY, NEV VARCHAR(255) NOT NULL UNIQUE, LEIRAS VARCHAR(255) NOT NULL, AR INTEGER NOT NULL, VAROS_ID INTEGER NOT NULL)";
+            cmd.ExecuteNonQuery();
+        }
+
+        private void btnVarosHozzaAd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbVnev.Text))
+            {
+                MessageBox.Show("Minden mezőt ki kell tölteni!");
+                return;
+            }
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"INSERT INTO varosok (nev, lakossag)
+                             VALUES (@nev,@lakossag)";
+            cmd.Parameters.AddWithValue("@nev", tbVnev.Text);
+            cmd.Parameters.AddWithValue("@lakossag", nUDLakossag.Value);
+
+            cmd.ExecuteNonQuery();
         }
     }
 }
